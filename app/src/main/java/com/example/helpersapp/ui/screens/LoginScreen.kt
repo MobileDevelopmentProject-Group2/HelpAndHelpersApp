5779124468fb62d1fe6df11b8e71463029157179
Cheng Shufen <ophelia.scarlett007@gmail.com>
2024-03-25 09:43:04 +0200

    Register screen and login screen front end code is ready add img folder

--- app/src/main/java/com/example/helpersapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/helpersapp/ui/screens/LoginScreen.kt
@@ -1,28 +1,137 @@
 package com.example.helpersapp.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.example.helpersapp.R
 
 @Composable
 fun LoginScreen(navController: NavController, usersViewModel: ViewModel) {
+    val localFocusManager = LocalFocusManager.current
+    var email by remember{ mutableStateOf("") }
+    var password by remember{ mutableStateOf("") }
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Login Screen"
+        Image(
+            painter = painterResource(id = R.mipmap.top_background) ,
+            contentDescription =null,
+            modifier = Modifier
+                .fillMaxWidth()
+
         )
+        Image(
+            painter = painterResource(id = R.mipmap.logo) ,
+            contentDescription =null,
+            modifier = Modifier
+                .height(180.dp)
+                //.padding(top=1.dp)
+                //.fillMaxWidth()
+
+        )
+        Text(
+            text = "Login" ,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.displayMedium
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = { Text(text = "Email") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text(text = "Password") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
+        )
+        Button(
+            onClick = {
+                // Handle the sign-up logic here
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Login")
+        }
+        TextButton(onClick = { navController.navigate("register") }) {
+            Text("Not a user? Register here",
+                style = MaterialTheme.typography.bodyLarge)
+        }
         Button(onClick = { navController.navigate("home")}) {
             Text(text = "Home")
         }
+        SodaLogo()
+    }
+
+
+}
+
+@Composable
+fun SodaLogo() {
+   val socialLogos = listOf(R.mipmap.facebook, R.mipmap.google, R.mipmap.twitter)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Absolute.Center
+    ) {
+
+//this iconbutton looks not right
+        IconButton(onClick = { /* TODO: Add action for Facebook */ }) {
+            Image(painter = painterResource(id = R.mipmap.facebook), contentDescription = "Facebook")
+        }
+        IconButton(onClick = { /* TODO: Add action for Facebook */ }) {
+            Image(painter = painterResource(id = R.mipmap.google), contentDescription = "Facebook")
+        }
+        IconButton(onClick = { /* TODO: Add action for Facebook */ }) {
+            Image(painter = painterResource(id = R.mipmap.twitter), contentDescription = "Facebook")
+        }
+        /*
+              socialLogos.forEach { logoRes ->
+            Image(
+                painter = painterResource(id = logoRes),
+                contentDescription = "Social media",
+                modifier = Modifier.size(48.dp)
+                )
+        }
+
+                IconButton(
+            onClick = { /* TODO: Add action for Facebook */ })
+        {
+            Icon(painter = painterResource(id = R.mipmap.facebook), contentDescription = "Facebook")
+        }
+        * */
     }
 }
--- app/src/main/java/com/example/helpersapp/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/helpersapp/ui/screens/RegisterScreen.kt
@@ -1,26 +1,133 @@
 package com.example.helpersapp.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.ModifierLocalProvider
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.example.helpersapp.R
+import androidx.compose.material3.Text as Text
+
+//import sun.tools.jstat.Alignment
 
 @Composable
 fun RegisterScreen(navController: NavController, usersViewModel: ViewModel) {
+    // below my code
+    // Add this to handle keyboard actions like dismissing the keyboard
+    val localFocusManager = LocalFocusManager.current
+    var username by remember{ mutableStateOf(" ")}
+    var fullname by remember{ mutableStateOf(" ")}
+    var email by remember{ mutableStateOf("")}
+    var password by remember{ mutableStateOf("")}
+    var address by remember{ mutableStateOf("")}
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        // add padding
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Image(
+            painter = painterResource(id = R.mipmap.top_background) ,
+            contentDescription =null,
+            modifier = Modifier
+                .fillMaxWidth()
+                //.height(150.dp)
+            )
+
         Text(
-            text = "Register Screen"
+            text = "Register",
+                    //below new code
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.displayMedium
         )
+        //Spacer(modifier = Modifier.height(200.dp))
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = {username = it},
+            label = { Text(text = "Username") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { localFocusManager.clearFocus() })
+            //keyboardActions = KeyboardActions(onNext = { localFocusManager.moveFocus(FocusDirection.Down) })
+        )
+        OutlinedTextField(
+            value = fullname,
+            onValueChange = {fullname = it},
+            label = { Text(text = "First name & last name") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { localFocusManager.clearFocus() })
+
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = { Text(text = "Email") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { localFocusManager.clearFocus() })
+
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text(text = "Password") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { localFocusManager.clearFocus() })
+
+        )
+        OutlinedTextField(
+            value = address,
+            onValueChange = {address = it},
+            label = { Text(text = "Address") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onNext = { localFocusManager.clearFocus() })
+        )
+        //Spacer(modifier = Modifier.height(100.dp))
+        Button(
+            onClick = {
+                // Handle the sign-up logic here
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Register")
+        }
+        TextButton(onClick = { navController.navigate("login") }) {
+            Text("Already a user? Login", style = MaterialTheme.typography.bodyMedium)
+        }
         Button(onClick = { navController.navigate("home")}) {
             Text(text = "Home")
         }
